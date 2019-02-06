474123bcdc49c33e070f36c0094f7d12605c2fa0
ohood84 <obeidat.ohood@gmail.com>
2019-02-06 09:38:02 -0800

    adding cipher page

---
+++ js/cipher1.js
@@ -0,0 +1,59 @@
+var str=(prompt("enter a sentence:"));
+L=str.length;
+alert(L);
+//
+ var toupcase = function(str,L){
+   str1=str.toUpperCase();
+  ch1=str1.charAt(0);
+  ch2=str1.charAt(L-1);
+    return  (ch1+ch2);
+ }
+
+//toupcase(str,L);
+
+ var reverse=function(strup){
+   ch3=strup.charAt(0);
+   ch4=strup.charAt(1);
+  return (ch4+ch3);
+ }
+// strup=toupcase(str,L);
+// reverse(strup);
+
+ var concatn=function(str,L){
+ st1=toupcase(str,L);
+ alert(st1);
+ alert(reverse(st1));
+   alert(str+reverse(st1));
+   return str+reverse(st1);
+ }
+//concatn(str,L);
+
+recode=function(str,L){
+s=(L/2);
+newstring=(str.charAt(s)+concatn(str,L));
+return newstring;}
+
+codestring=recode(str,L);
+alert(codestring);
+
+reversestring=function(str){
+
+    return (str.split("").reverse().join(""));
+
+}
+
+finalstring=reversestring(codestring);
+alert(finalstring);
+
+$(function(){
+$("img/show.jpg").click(function(){
+  alert(str);
+$("img/show.png").hide();
+
+});
+
+$("img").click(function(){
+
+  alert(finalstring);
+});
+});
